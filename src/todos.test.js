dea275d0e192d0005d26ab8b4036a20fa3bb19e4
davidyang2149 <davidyang2149@gmail.com>
2020-12-22 21:31:14 +0900

    Update naming change
    Todo Javascript.
    - naming change.

--- src/todos.test.js
+++ src/todos.test.js
@@ -1,22 +1,21 @@
-
 describe('Todos', () => {
-  test('Check addTodo able add todo to todoList', () => {
+  test('Check addTodo able add todo to todos', () => {
     document.body.innerHTML = `
     <input id="newTodo" />
     <button id="addTodo">Add todo</button>
-    <ol id="todoList"></ol>
+    <ol id="todos"></ol>
     `;
 
     const newTodo = document.getElementById('newTodo');
     const addTodo = document.getElementById('addTodo');
-    const todolist = document.getElementById('todoList');
+    const todolist = document.getElementById('todos');
 
     require('./todos.js');
 
-    newTodo.value = 'New todolist!';
+    newTodo.value = 'New todo!';
     addTodo.click();
 
-    expect(todolist.innerHTML).toBe('<li>New todolist!</li>');
+    expect(todolist.innerHTML).toBe('<li>New todo!</li>');
   });
 
 });
