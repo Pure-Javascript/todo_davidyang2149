8b46f7336490ae44808e15e78fbdb8304a5257e2
davidyang2149 <davidyang2149@gmail.com>
2020-12-25 21:08:15 +0900

    Red Update todos.test.js(Blank)
    Todo Javascript.
    - Red: Update Test Code.
    - Blank check: title & content.

--- src/todos.test.js
+++ src/todos.test.js
@@ -1,32 +1,67 @@
+const context = global;
+
 describe('Todos', () => {
-  test('Check addTodo able add todo to todos', () => {
-    document.body.innerHTML = `
-    <div>
-      <label for="title">Title</label>
-      <input type="text" id="title" />
-    </div>
-    <div>
-      <label for="content">Content</label>
-      <input type="text" id="content" />
-    </div>
+  describe('with title & content', () => {
+    test('addTodo into todos', () => {
+      document.body.innerHTML = `
+      <div>
+        <label for="title">Title</label>
+        <input type="text" id="title" />
+      </div>
+      <div>
+        <label for="content">Content</label>
+        <input type="text" id="content" />
+      </div>
+  
+      <button id="addTodo">Add todo</button>
+      <ol id="todos"></ol>
+      `;
 
-    <button id="addTodo">Add todo</button>
-    <ol id="todos"></ol>
-    `;
+      const title = document.getElementById('title');
+      const content = document.getElementById('content');
+      const addTodo = document.getElementById('addTodo');
+      const todos = document.getElementById('todos');
 
-    const title = document.getElementById('title');
-    const content = document.getElementById('content');
-    const addTodo = document.getElementById('addTodo');
-    const todos = document.getElementById('todos');
+      require('./todos.js');
 
-    require('./todos.js');
+      title.value = 'New todo title!';
+      content.value = 'New todo content!';
 
-    title.value = 'New todo title!';
-    content.value = 'New todo content!';
+      addTodo.click();
 
-    addTodo.click();
-
-    expect(todos.innerHTML).toBe('<li>Title: New todo title! / Content: New todo content!</li>');
+      expect(todos.innerHTML).toBe('<li>Title: New todo title! / Content: New todo content!</li>');
+    });
   });
 
+  describe('without title & content', () => {
+    test('addTodo into todos', () => {
+      document.body.innerHTML = `
+      <div>
+        <label for="title">Title</label>
+        <input type="text" id="title" />
+      </div>
+      <div>
+        <label for="content">Content</label>
+        <input type="text" id="content" />
+      </div>
+  
+      <button id="addTodo">Add todo</button>
+      <ol id="todos"></ol>
+      `;
+
+      const title = document.getElementById('title');
+      const content = document.getElementById('content');
+      const addTodo = document.getElementById('addTodo');
+      const todos = document.getElementById('todos');
+
+      require('./todos.js');
+
+      title.value = '';
+      content.value = '';
+
+      addTodo.click();
+
+      expect(todos.innerHTML).not.toBe('<li>Title: New todo title! / Content: New todo content!</li>');
+    });
+  });
 });
