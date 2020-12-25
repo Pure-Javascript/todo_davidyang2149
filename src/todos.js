fc236d018edfad9f9242084a2231e6e7c43b11bc
davidyang2149 <davidyang2149@gmail.com>
2020-12-25 21:11:35 +0900

    Green Update todos.js(Blank)
    Todo Javascript.
    - Green: Update Code.
    - Blank Check: title & content.

--- src/todos.js
+++ src/todos.js
@@ -1,8 +1,22 @@
 const getId = (value) => document.getElementById(value);
 
+const isBlank = (target) => getId(target).value.trim().length === 0;
+const getFocus = (target) => getId(target).focus();
+
 const addTodo = () => {
-  const newTodoTitle = getId('title');
-  const newTodoContent = getId('content');
+  const title = 'title';
+  const content = 'content';
+
+  if (isBlank(title)) {
+    return getFocus(title);
+  }
+
+  if (isBlank(content)) {
+    return getFocus(content);
+  }
+
+  const newTodoTitle = getId(title);
+  const newTodoContent = getId(content);
 
   let currentTodos = getId('todos').innerHTML;
   currentTodos += `<li>Title: ${newTodoTitle.value} / Content: ${newTodoContent.value}</li>`;
