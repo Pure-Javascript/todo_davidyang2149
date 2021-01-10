0c6167ffdf0a12cb40414ac179af63cd30a6ca8e
davidyang2149 <davidyang2149@gmail.com>
2021-01-10 12:09:30 +0900

    Update todos.js & Add style.css(editTodo)
    Todo Javascript.
    - todos: editTodo Add.
    - style css Add.

--- src/todos.js
+++ src/todos.js
@@ -23,12 +23,21 @@ const setTodoStorage = setLocalStorage(toStringify)('todos');
     // map에서 forEach로 변경
     initTodos.forEach((todo) => todos += `
       <li id=${todo.id}>
-        <div>
+        <div class="viewPage">
           Title: ${todo.title} / Content: ${todo.content}
           <label>
-            <input class="done" type="checkbox" ${todo.done && 'checked'} data-id=${todo.id} />
-            Complete
+          <input class="done" type="checkbox" ${todo.done && 'checked'} data-id="${todo.id}" />
+          Complete
           </label>
+          <button class="open" data-id="${todo.id}">Edit</button>
+        </div>
+        <div class="editPage hidden">
+          <label for="title-${todo.id}">Title</label>
+          <input type="text" id="title-${todo.id}" value="${todo.title}" />
+          <label for="content-${todo.id}">Content</label>
+          <input type="text" id="content-${todo.id}" value="${todo.content}" />
+          <button class="edit" data-id="${todo.id}">Update</button>
+          <button class="close" data-id="${todo.id}">Close</button>
         </div>
       </li>
     `);
@@ -41,6 +50,15 @@ const setTodoStorage = setLocalStorage(toStringify)('todos');
 
     const doneButton = document.querySelectorAll('.done');
     doneButton.forEach(done => done.addEventListener('click', () => completeTodo(done.getAttribute('data-id'))));
+
+    const openEditButton = document.querySelectorAll('.open');
+    openEditButton.forEach(done => done.addEventListener('click', () => openEdit(done.getAttribute('data-id'))));
+
+    const closeEditButton = document.querySelectorAll('.close');
+    closeEditButton.forEach(done => done.addEventListener('click', () => closeEdit(done.getAttribute('data-id'))));
+
+    const editButton = document.querySelectorAll('.edit');
+    editButton.forEach(done => done.addEventListener('click', () => updateTodo(done.getAttribute('data-id'))));
   }
 
   // add Todo
@@ -81,6 +99,50 @@ const setTodoStorage = setLocalStorage(toStringify)('todos');
     setTodoStorage(
       initTodos.map((todo) => todo.id === id ? { ...todo, done: !todo.done } : todo)
     );
+
+    loadTodos();
+  }
+
+  // open edit
+  const openEdit = (id) => {
+    getSelector('.viewPage').forEach(edit => edit.classList.remove('hidden'));
+    getSelector('.editPage').forEach(edit => edit.classList.add('hidden'));
+
+    getId(id).childNodes[1].classList.add('hidden');
+    getId(id).childNodes[3].classList.remove('hidden');
+  }
+
+  const closeEdit = (id) => {
+    getId(id).childNodes[1].classList.remove('hidden');
+    getId(id).childNodes[3].classList.add('hidden');
+  }
+
+  // update task
+  const updateTodo = (id) => {
+    const title = `title-${id}`;
+    const content = `content-${id}`;
+
+    if (isBlank(title)) {
+      return getFocus(title);
+    }
+
+    if (isBlank(content)) {
+      return getFocus(content);
+    }
+
+    const titleId = getId(title);
+    const contentId = getId(content);
+
+    const initTodos = toJSON(getLocalStorage('todos')) || [];
+    setTodoStorage(initTodos.map(todo => todo.id === id ? {
+      ...todo,
+      title: titleId.value,
+      content: contentId.value,
+    } : todo)
+    );
+
+    titleId.value = '';
+    contentId.value = '';
 
     loadTodos();
   }
